acdf38ec5cf62b0652cdd824e97207ffb5c23629
Daniel <humbertodanielcaballeros@gmail.com>
2024-09-15 19:30:33 -0500

    listo

--- README.md
+++ README.md
@@ -1,14 +1,55 @@
-# Welcome to your CDK TypeScript project
+# Proyecto 1 de Cloud computing (typescript)
 
-This is a blank project for CDK development with TypeScript.
+## Requisitos previos
 
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
+Antes de comenzar, asegúrate de tener instalados los siguientes requisitos:
 
-## Useful commands
+1. **AWS CLI**: Configurado con tus credenciales de acceso.
+2. **AWS CDK**: Instalar usando el siguiente comando:
 
-* `npm run build`   compile typescript to js
-* `npm run watch`   watch for changes and compile
-* `npm run test`    perform the jest unit tests
-* `npx cdk deploy`  deploy this stack to your default AWS account/region
-* `npx cdk diff`    compare deployed stack with current state
-* `npx cdk synth`   emits the synthesized CloudFormation template
+   ```bash
+   npm install -g aws-cdk
+
+## Pasos a seguir:
+inicializamos el aws
+
+guadamos las credenciales en el directorio de .aws con el siguiente comando 
+
+   ```bash
+   notepad $env:USERPROFILE.aws\credentials
+
+creamos un nuveo bucket 
+![Creación del bucket](./images/1.png)
+
+buscamos nuestra id del AMI
+
+buscamos tambien la id del panel VPC
+![](./images/2.png)
+
+
+Creamos un archivo en vs de typescript con los sigueintes comandos 
+
+   ```bash
+   npm install -g aws-cdk
+
+   cdk init app --language typescript
+
+Ajustamos el codigo de tal manera que utilice las credenciales y tenaga los roles establecudos para que pueda abirir los puertos 8080 y 8081
+
+Usamos los siguietes comando para deployar
+
+   ```bash
+   aws ssm put-parameter --name "/cdk-bootstrap/hnb659fds/version" --type "String" --value "15" --overwrite
+
+   cdk deploy
+
+Comporbamos que se haya deployado una nueva instancia en nuestro ec2
+![](./images/3.png)
+
+
+Uilizamos el id de esta instancia y comprobamos con los puertos establecidos
+
+![](./images/4.png)
+![](./images/5.png)
+
+
--- lib/proyecto1-stack.ts
+++ lib/proyecto1-stack.ts
@@ -1,16 +1,63 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 
 export class Proyecto1Stack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        const synthesizer = new cdk.DefaultStackSynthesizer({
+            fileAssetsBucketName: "proyecto-cloud",  // Tu bucket
+            bucketPrefix: "",
+            cloudFormationExecutionRole: "arn:aws:iam::298526054328:role/LabRole",  // Tu IAM Role
+            deployRoleArn: "arn:aws:iam::298526054328:role/LabRole",
+            fileAssetPublishingRoleArn: "arn:aws:iam::298526054328:role/LabRole",
+            imageAssetPublishingRoleArn: "arn:aws:iam::298526054328:role/LabRole"
+        });
 
-    // The code that defines your stack goes here
+        // Combina props con synthesizer
+        super(scope, id, {
+            synthesizer: synthesizer,
+            ...props
+        });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'Proyecto1Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+        // VPC existente
+        const vpc = ec2.Vpc.fromLookup(this, "MyVpc", { vpcId: "vpc-05190e608cebfb9be" });  // Tu VPC ID
+
+        // IAM Role existente
+        const instanceRole = iam.Role.fromRoleArn(this, "LabRole", "arn:aws:iam::298526054328:role/LabRole");
+
+        // Usar LookupMachineImage para encontrar la última imagen de Ubuntu
+        const ubuntuAmi = new ec2.LookupMachineImage({
+            name: "ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*",
+            owners: ["099720109477"]  // Propietario de Ubuntu
+        });
+
+        // Crear una instancia con los parámetros personalizados
+        const instance = new ec2.Instance(this, "WebServerProyecto1", {
+            instanceType: new ec2.InstanceType("t3.micro"),  // Cambié t2.micro a t3.micro para mejorar el rendimiento
+            machineImage: ubuntuAmi,
+            vpc: vpc,
+            role: instanceRole
+        });
+
+        // Comandos de UserData
+        const userDataCommands = [
+            "apt-get update -y",
+            "apt-get install -y git",
+            "git clone https://github.com/DANIIcs/websimple.git",
+            "git clone https://github.com/DANIIcs/webplantilla.git",
+            "cd websimple",
+            "nohup python3 -m http.server 8080 &",
+            "cd ../webplantilla",
+            "nohup python3 -m http.server 8081 &"
+        ];
+
+        // Añadir los comandos de UserData
+        userDataCommands.forEach(cmd => instance.userData.addCommands(cmd));
+
+        // Permitir tráfico HTTP en los puertos 8080 y 8081 desde cualquier IPv4
+        instance.connections.allowFromAnyIpv4(ec2.Port.tcp(8080), "Allow HTTP traffic on port 8080");
+        instance.connections.allowFromAnyIpv4(ec2.Port.tcp(8081), "Allow HTTP traffic on port 8081");
+    }
 }
